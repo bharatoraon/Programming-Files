e105d4b16347baef05281e7305bdad4e5876f3e7
Bharat Oraon <59952059+bharatoraon@users.noreply.github.com>
2021-01-18 18:11:45 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Unsigned Numbers
+//
+//  Created by Bharat Oraon on 18/01/21.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char a = 114;
+    cout << a << endl;
+    unsigned int b = -123;
+    cout << b << endl;
+}
